9b2a8f435d3874f99853232ae0451f1f0fd1fde7
vkowalski <vkowalski@lab-code.com>
2021-02-15 17:51:02 +0300

    initial commit

---
+++ app/src/main/java/com/victor/ko/kotlin_shopping_cart_loadmore/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.victor.ko.kotlin_shopping_cart_loadmore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
